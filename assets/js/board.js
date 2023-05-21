26bbd4d2acfb5712fb97935b1e32df969c927316
orianig <oriana.ig016@gmail.com>
2023-05-21 15:43:20 +0200

    feat: add interaction functions between pages

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,5 +1,8 @@
-import { Player, Game, LocalStorage } from "./models";
-
+import {LocalStorage } from "./models.js";
 
 const localStorage = new LocalStorage();
 localStorage.getItem('player');
+
+let player = storage.getItem('mind-player');
+console.log(player);
+
---
+++ assets/js/level.js
@@ -0,0 +1,47 @@
+let level = parseInt(localStorage.getItem('mind-level'));
+
+// Selectores DOM
+const level1 = document.querySelector('.level-1');
+const level2 = document.querySelector('.level-2');
+const level3 = document.querySelector('.level-3');
+const btnSave = document.getElementById('btnSave');
+
+
+if (level) {
+    if (level === 1) {
+        level1.classList.add('active')
+    }
+    if (level === 2) {
+        level2.classList.add('active')
+    }
+    if (level === 3) {
+        level3.classList.add('active')
+    }
+} else {
+    level = 1
+}
+
+//para el background de los elementos segun seleccion
+level1.addEventListener("click", () => {
+    level = 1
+    level1.classList.add('active')
+    level2.classList.remove('active')
+    level3.classList.remove('active')
+});
+level2.addEventListener("click", () => {
+    level = 2
+    level1.classList.remove('active')
+    level2.classList.add('active')
+    level3.classList.remove('active')
+});
+level3.addEventListener("click", () => {
+    level = 3
+    level1.classList.remove('active')
+    level2.classList.remove('active')
+    level3.classList.add('active')
+});
+//guardado de la eleccion y paso a la siguiente pagina
+btnSave.addEventListener("click", () => {
+    localStorage.setItem('mind-level', level);
+    window.location.href = './color.html';
+});

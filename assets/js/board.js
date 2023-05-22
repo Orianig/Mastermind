a5e9f08fd8fe4d93ef4c9e33e18805cae5284f60
orianig <oriana.ig016@gmail.com>
2023-05-23 01:17:35 +0200

    refactor: change the board functions - modify the js for better development

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,46 +1,107 @@
+
 //datos del jugador - nombre y avatar / level/ colores
 let player = parseInt(localStorage.getItem('mind-player'));
 let level = parseInt(localStorage.getItem('mind-level'));
-let colorballs = parseInt(localStorage.getItem('mind-colors'));
+let colorballs = JSON.parse(localStorage.getItem('mind-colors'));
+
 //elementos DOM
-const container = document.querySelector('.color-balls-container');
 const containerPlayer = document.querySelector('.color-balls-container-player');
+const rowBoard = document.querySelector('.board');
 
 // Obtencion de la cantidad de bolas segun el nivel
-// filas del tablero
 const numBallsLevel1 = 4;
 const numBallsLevel2 = 5;
 const numBallsLevel3 = 6;
-const addBalls = (numBalls) => {
-    for (let i = 0; i < numBalls; i++) {
-        const ball = document.createElement('div');
-        ball.classList.add('ball');
-        container.appendChild(ball);
-    };
+
+// Obtencion de la cantidad de bolas segun el nivel
+const numRowsLevel1 = 10;
+const numRowsLevel2 = 8;
+const numRowsLevel3 = 6;
+
+//guardado de cada color al hacer clic, array que permite el bucle
+const colorballsCounterClick = [0, 0, 0, 0, 0, 0];
+
+const addRows = (numRows) => {
+    for (let i = 0; i < numRows; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row-ppal');
+
+        const points = document.createElement('div');
+        points.classList.add('points');
+
+        for (let j = 0; j < 4; j++) {
+            const ball = document.createElement('span');
+            ball.classList.add('ball');
+            points.appendChild(ball);
+        }
+        row.appendChild(points);
+
+        const ballsContainer = document.createElement('div');
+        ballsContainer.classList.add('color-balls-container');
+
+
+        const addBalls = (numBalls) => {
+            for (let i = 0; i < numBalls; i++) {
+                const ball = document.createElement('div');
+                ball.classList.add('ball');
+                ballsContainer.appendChild(ball);
+
+                ball.addEventListener("click", () => {
+                    console.log(colorballs[colorballsCounterClick[i]]);
+                    ball.style.backgroundColor = colorballs[colorballsCounterClick[i]];
+                    if (colorballsCounterClick[i] < numBalls - 1) {
+                        colorballsCounterClick[i]++;
+                    } else {
+                        colorballsCounterClick[i] = 0;
+                    }
+                    console.log(colorballsCounterClick, colorballs)
+                });
+            }
+        };
+        if (level) {
+            if (level === 1) {
+                addBalls(numBallsLevel1);
+            }
+            if (level === 2) {
+                addBalls(numBallsLevel2);
+            }
+            if (level === 3) {
+                addBalls(numBallsLevel3);
+            }
+        } else {
+            addBalls(numBallsLevel1);
+        };
+        row.appendChild(ballsContainer);
+
+        const validation = document.createElement('button');
+        validation.classList.add('validation-button');
+        row.appendChild(validation);
+
+        rowBoard.appendChild(row);
+    }
 };
 if (level) {
     if (level === 1) {
-        addBalls(numBallsLevel1);
+        addRows(numRowsLevel1);
     }
     if (level === 2) {
-        addBalls(numBallsLevel2);
+        addRows(numRowsLevel2);
     }
     if (level === 3) {
-        addBalls(numBallsLevel3);
+        addRows(numRowsLevel3);
     }
 } else {
-    addBalls(numBallsLevel1);
+    addRows(numRowsLevel1);
 };
 
-// Obtencion de la cantidad de bolas segun el nivel
-// fila de color
 const addBallsPlayer = (numBalls) => {
     for (let i = 0; i < numBalls; i++) {
         const ballPlayer = document.createElement('div');
         ballPlayer.classList.add('ballPlayer');
+        ballPlayer.style.backgroundColor = colorballs[i];
         containerPlayer.appendChild(ballPlayer);
-    };
-};
+    }
+}
 if (level) {
     if (level === 1) {
         addBallsPlayer(numBallsLevel1);
@@ -55,4 +116,3 @@ if (level) {
     addBallsPlayer(numBallsLevel1);
 };
 
-//obtencion del color

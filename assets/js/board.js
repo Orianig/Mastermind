b6c57e068d78295590d1c3313aea7fe1550522d5
orianig <oriana.ig016@gmail.com>
2023-05-25 00:53:07 +0200

    feat: add play validation - score points - winning move

--- assets/js/board.js
+++ assets/js/board.js
@@ -25,7 +25,7 @@ const selectedRow = numRowDictionary[level]
 const selectedCol = numColumsDictionary[level]
 
 // Generador de la jugada ganadora
-const secretColorList = (colorList) => {
+const getSecretColorList = (colorList) => {
     let secretColors = [];
 
     for (let i = 0; i < colorList.length; i++) {
@@ -38,14 +38,50 @@ const secretColorList = (colorList) => {
     return secretColors;
 };
 
-const secretColorsResult = secretColorList(colorballs);
-localStorage.setItem('winnerColors', JSON.stringify(secretColorsResult));
+const secretColorsList = getSecretColorList(colorballs);
+localStorage.setItem('winnerColors', JSON.stringify(secretColorsList));
 
+
+let colorMatrix = new Array(selectedRow);
+for (let i = 0; i < selectedRow; i++) {
+    colorMatrix[i] = new Array(selectedCol).fill(-1);
+}
+
+let validationRowList = new Array(selectedRow);
+validationRowList.fill(false);
+validationRowList[0] = true;
+
+
+// Comprobacion de fichas blancas y negras
+const calculateMatches = (secretColors, userColors) => {
+    const matches = [];
+    const secretColorsAux = [...secretColors];
+
+    for (let i = 0; i < userColors.length; i++) {
+        const userColor = userColors[i];
+        let match = '';
+
+        // Verificar coincidencia exacta
+        if (userColor === secretColorsAux[i]) {
+            match = 'negro';
+            secretColorsAux[i] = null; // Marcar el color como usado en secretColors
+        } else {
+            // Verificar coincidencia por tipo
+            const index = secretColorsAux.findIndex((color) => color === userColor);
+            if (index !== -1) {
+                match = 'blanco';
+                secretColorsAux[index] = null; // Marcar el color como usado en secretColors
+            }
+        }
+        matches.push(match);
+    }
+    return matches;
+};
 
 
 //generador de cada una de las filas que me venganpor nivel asignado
 const generateRows = (numRows) => {
-    for (let i = 0; i < numRows; i++) {
+    for (let indexRow = 0; indexRow < numRows; indexRow++) {
         const rowContainer = document.createElement('div');
         rowContainer.classList.add('row-ppal');
 
@@ -72,22 +108,26 @@ const generateRows = (numRows) => {
 
         //genera cada una de las bolas del tablero
         const generateColumns = (numBalls) => {
-            for (let k = 0; k < numBalls; k++) {
+            for (let indexColumn = 0; indexColumn < numBalls; indexColumn++) {
+
                 const colBalls = document.createElement('div');
                 colBalls.classList.add('ball');
                 columnContainer.appendChild(colBalls);
 
                 //asignacion del color de cada bola
                 colBalls.addEventListener("click", () => {
-                    if (rowColorIndexList[k] < numBalls - 1) {
-                        rowColorIndexList[k]++;
+                    if (!validationRowList[indexRow]) {
+                        alert('No puedes jugar esta fila aun');
+                        return
+                    }
+                    if (colorMatrix[indexRow][indexColumn] < numBalls - 1) {
+                        colorMatrix[indexRow][indexColumn]++;
                         //si me alcanza el ultimo color disponible vuelve a la posicion 0
                     } else {
-                        rowColorIndexList[k] = 0;
+                        colorMatrix[indexRow][indexColumn] = 0;
                     }
-                    console.log(colorballs[rowColorIndexList[k]]);
                     //   índice para acceder al color específico en la matriz colorballs    
-                    colBalls.style.backgroundColor = colorballs[rowColorIndexList[k]];
+                    colBalls.style.backgroundColor = colorballs[colorMatrix[indexRow][indexColumn]];
                 });
             }
         };
@@ -96,18 +136,60 @@ const generateRows = (numRows) => {
         };
         rowContainer.appendChild(columnContainer);
 
-        const validation = document.createElement('button');
-        validation.classList.add('validation-button');
-        rowContainer.appendChild(validation);
+        // Manejador del botón
+        const validationButton = document.createElement('button');
+        validationButton.classList.add('validation-button');
+        validationButton.innerText = "Validar";
+        rowContainer.appendChild(validationButton);
+        validationButton.addEventListener("click", () => {
+            const isCompleteRow = colorMatrix[indexRow].every(item => item >= 0);
+            if (!validationRowList[indexRow]) {
+                alert('No puedes validar esta fila aun');
+                return
+            }
+            if (!isCompleteRow) {
+                alert('No puedes validar si no has anadido todos los colores');
+                return;
+            }
+            const colorHexaRowList = colorMatrix[indexRow].map(item => colorballs[item]);
+            let matches = calculateMatches(secretColorsList, colorHexaRowList);
+            matches = matches.filter(item => item);
 
-        rowBoard.appendChild(rowContainer);
+            let scorePoints = containerPointScore.querySelectorAll('.pointScore');
+            for (i = 0; i < scorePoints.length; i++) {
+                const match = matches[i];
+                if (match === 'negro') {
+                    scorePoints[i].style.backgroundColor = 'black';
+                    scorePoints[i].classList.add('black');
+                }
+                if (match === 'blanco') {
+                    scorePoints[i].style.backgroundColor = 'white';
+                    scorePoints[i].classList.add('white');
+                }
+            }
+
+            const isWinSet = matches.every(item => item == 'negro');
+            if (isWinSet) {
+                alert('G A N A S T E');
+                window.location.href = './winner.html';
+                return;
+            }
+            validationRowList[indexRow] = false;
+            if (indexRow < numRows - 1) {
+                validationRowList[indexRow + 1] = true;
+            } else {
+                alert('P E R D I S T E');
+            }
+        })
+
+        rowBoard.insertBefore(rowContainer, rowBoard.firstChild);
     }
 };
 if (level) {
     generateRows(selectedRow);
 };
 
-const generateColsBallsPlayer = (numBalls) => {
+const generateBallsPlayer = (numBalls) => {
     for (let i = 0; i < numBalls; i++) {
         const ballPlayer = document.createElement('div');
         ballPlayer.classList.add('ballPlayer');
@@ -116,7 +198,7 @@ const generateColsBallsPlayer = (numBalls) => {
     }
 }
 if (level) {
-    generateColsBallsPlayer(selectedCol);
+    generateBallsPlayer(selectedCol);
 };
 
 

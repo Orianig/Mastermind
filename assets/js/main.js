ba8bee6a0c19a073017505a2f95023bc952d0570
orianig <oriana.ig016@gmail.com>
2023-05-25 23:28:58 +0200

    style: change in edge details - shadows and elements in general

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,21 @@ const btnPlay = document.getElementById('play-button');
 btnPlay.addEventListener("click", () => {
     window.location.href = './pages/register.html';
 });
+
+//Elementos DOM
+const audioPlayer = document.getElementById('audioPlayer');
+const toggleSoundButton = document.getElementById('sound-button');
+const soundImage = document.getElementById('soundImage');
+
+function toggleSound() {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        soundImage.src = '../assets/img/sound-on.png'; // Cambiar a la imagen de sonido activado
+    } else {
+        audioPlayer.pause();
+        soundImage.src = '../assets/img/sound-off.png'; // Cambiar a la imagen de sonido desactivado
+    }
+}
+
+// Evento clic del botón para activar/desactivar el sonido y cambiar la imagen
+toggleSoundButton.addEventListener('click', toggleSound);
